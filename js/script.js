89eee348a83dfc009ba702a7ef5af62366878994
realjema <realjema@gmail.com>
2020-03-27 17:06:43 +0100

    fetch and update functions written

--- js/script.js
+++ js/script.js
@@ -7,160 +7,201 @@ let total_cases = document.getElementById("total_cases");
 let local_death = document.getElementById("local_death");
 let local_recovered = document.getElementById("local_recovered");
 let local_cases = document.getElementById("local_cases");
-let table = document.getElementById('countries_stat')
+let table = document.getElementById("countries_stat");
 // Fetching the Data from the server
 
-function Update_Database(){
-    // This section updates the global statistics 
-    //Fetching the World Data
-    fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-            "x-rapidapi-key": "53009286a0mshdc8ec356f7aa205p1e0e80jsn5858f548ed53"
-        }
-    })
-    
-    .then(response => response.json().then( data => {
-        // sending the data to my personal api 
-        (async () => {
-            const rawResponse = await fetch('http:localhost:5000/api/update', {
-              method: 'POST',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(cmr_data)
-            });
-            const content = await rawResponse;
-            console.log(content);
-          })();
-        // console.log(data);
-        total_cases.innerHTML = data.total_cases;
-        // new_cases.innerHTML = data.new_cases;
-        // new_death.innerHTML = data.new_deaths;
-        total_death.innerHTML = data.total_deaths;
-        total_recovered.innerHTML = data.total_recovered;
-    
-    })).catch(err => {
-        console.log(err);
-    });
-
-//Fetching The Case by Country Data
-fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
-    "method": "GET",
-    "headers": {
+function update_Database() {
+  // This section updates the global statistics
+  //Fetching the World Data from rapid api servers 
+//   this fucntion gets the global statistics 
+  fetch(
+    "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php",
+    {
+      method: "GET",
+      headers: {
         "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
         "x-rapidapi-key": "53009286a0mshdc8ec356f7aa205p1e0e80jsn5858f548ed53"
+      }
     }
-})
-.then(response => response.json().then(data =>{
-    // console.log(data)
-    let countries_stat = data.countries_stat;
-    let list_country = document.getElementById("country_stats");
-//Getting all the country statistic using a loop
-    for(let i = 0; i<countries_stat.length;i++){
-        if(countries_stat[i].country_name == 'Cameroon'){
-           
-            // updating value in api 
+  )
+    .then(response =>
+      response.json().then(data => {
+        // sending the data to my personal api
+        // flask
+        (async () => {
+          const rawResponse = await fetch(
+            "http:localhost:5000/api/update/global",
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(data)
+            }
+          );
+          const content = await rawResponse;
+        //   console.log(content);
+        })();
+      })
+    )
+    .catch(err => {
+      console.log(err);
+    });
+
+  //Fetching The Case by Country Data
+//   I get all the data and filter through using a for loop
+  fetch(
+    "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "53009286a0mshdc8ec356f7aa205p1e0e80jsn5858f548ed53"
+      }
+    }
+  )
+    .then(response =>
+      response.json().then(data => {
+        // console.log(data)
+        let countries_stat = data.countries_stat;
+        //Getting all the country statistic using a loop
+        for (let i = 0; i < countries_stat.length; i++) {
+          if (countries_stat[i].country_name == "Cameroon") {
+            // updating value in my personal api 
             var cmr_data = countries_stat[i];
             (async () => {
-                const rawResponse = await fetch('http:localhost:5000/api/update/local', {
-                  method: 'POST',
+              const rawResponse = await fetch(
+                "http:localhost:5000/api/update/local",
+                {
+                  method: "POST",
                   headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
                   },
                   body: JSON.stringify(cmr_data)
-                });
-                const content = await rawResponse;
-                console.log(content);
-              })();
-            local_cases.innerHTML = countries_stat[i].cases;
-            local_death.innerHTML = countries_stat[i].deaths;
-            local_recovered.innerHTML = countries_stat[i].total_recovered;
+                }
+              );
+              const content = await rawResponse;
+            //   console.log(content);
+            })();
+          }
         }
-
-    }
-}))
-.catch(err => {
-    console.log(err);
-});
-
-}
-(async () => {
-    const rawResponse = await fetch('http:localhost:5000/api/data', {
-      method: 'GET',
+      })
+    )
+    .catch(err => {
+      console.log(err);
     });
-    const content = await rawResponse.json();
-    console.log(content);
-  })();
+}
+
+// getting the data from the local api and displaying it
+function fetch_Data(){
+    (async () => {
+      const rawResponse = await fetch("http:localhost:5000/api/data", {
+        method: "GET"
+      });
+
+      // implement a local file fetch in case your own api fails 
+
+      
+      const content = await rawResponse.json();
+    //   console.log(content);
+    
+      // displaying data on the view
+      total_cases.innerHTML = content["glb_stat"].total_cases;
+      total_death.innerHTML = content["glb_stat"].total_deaths;
+      total_recovered.innerHTML = content["glb_stat"].total_recovered;
+      local_cases.innerHTML = content["cmr_stat"].cases;
+      local_death.innerHTML = content["cmr_stat"].deaths;
+      local_recovered.innerHTML = content["cmr_stat"].total_recovered;
+    })();
+}
+
+
+update_Database()
+fetch_Data()
 
 
 
 
 
 
-
-
-var map = L.map('map', {
-    center: [7.3, 12.3],
-    zoom: 6
+var map = L.map("map", {
+  center: [7.3, 12.3],
+  zoom: 6
 });
 
-var defaultLayer = L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+var defaultLayer = L.tileLayer.provider("OpenStreetMap.Mapnik").addTo(map);
 
 // base layers
 var baseLayers = {
-    'OpenStreetMap Default': defaultLayer,
-    'OpenStreetMap German Style': L.tileLayer.provider('OpenStreetMap.DE'),
-    'OpenStreetMap Black and White': L.tileLayer.provider('OpenStreetMap.BlackAndWhite'),
-    'OpenStreetMap H.O.T.': L.tileLayer.provider('OpenStreetMap.HOT'),
-    'Thunderforest OpenCycleMap': L.tileLayer.provider('Thunderforest.OpenCycleMap'),
-    'Thunderforest Transport': L.tileLayer.provider('Thunderforest.Transport'),
-    'Thunderforest Landscape': L.tileLayer.provider('Thunderforest.Landscape'),
-    'Hydda Full': L.tileLayer.provider('Hydda.Full'),
-    'Stamen Toner': L.tileLayer.provider('Stamen.Toner'),
-    'Stamen Terrain': L.tileLayer.provider('Stamen.Terrain'),
-    'Stamen Watercolor': L.tileLayer.provider('Stamen.Watercolor'),
-    'Esri WorldStreetMap': L.tileLayer.provider('Esri.WorldStreetMap'),
-    'Esri DeLorme': L.tileLayer.provider('Esri.DeLorme'),
-    'Esri WorldTopoMap': L.tileLayer.provider('Esri.WorldTopoMap'),
-    'Esri WorldImagery': L.tileLayer.provider('Esri.WorldImagery'),
-    'Esri WorldTerrain': L.tileLayer.provider('Esri.WorldTerrain'),
-    'Esri WorldShadedRelief': L.tileLayer.provider('Esri.WorldShadedRelief'),
-    'Esri WorldPhysical': L.tileLayer.provider('Esri.WorldPhysical'),
-    'Esri OceanBasemap': L.tileLayer.provider('Esri.OceanBasemap'),
-    'Esri NatGeoWorldMap': L.tileLayer.provider('Esri.NatGeoWorldMap'),
-    'Esri WorldGrayCanvas': L.tileLayer.provider('Esri.WorldGrayCanvas'),
-    'Geoportail France Maps': L.tileLayer.provider('GeoportailFrance'),
-    'Geoportail France Orthos': L.tileLayer.provider('GeoportailFrance.orthos'),
-    'Geoportail France classic maps': L.tileLayer.provider('GeoportailFrance.ignMaps')
+  "OpenStreetMap Default": defaultLayer,
+  "OpenStreetMap German Style": L.tileLayer.provider("OpenStreetMap.DE"),
+  "OpenStreetMap Black and White": L.tileLayer.provider(
+    "OpenStreetMap.BlackAndWhite"
+  ),
+  "OpenStreetMap H.O.T.": L.tileLayer.provider("OpenStreetMap.HOT"),
+  "Thunderforest OpenCycleMap": L.tileLayer.provider(
+    "Thunderforest.OpenCycleMap"
+  ),
+  "Thunderforest Transport": L.tileLayer.provider("Thunderforest.Transport"),
+  "Thunderforest Landscape": L.tileLayer.provider("Thunderforest.Landscape"),
+  "Hydda Full": L.tileLayer.provider("Hydda.Full"),
+  "Stamen Toner": L.tileLayer.provider("Stamen.Toner"),
+  "Stamen Terrain": L.tileLayer.provider("Stamen.Terrain"),
+  "Stamen Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
+  "Esri WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
+  "Esri DeLorme": L.tileLayer.provider("Esri.DeLorme"),
+  "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+  "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+  "Esri WorldTerrain": L.tileLayer.provider("Esri.WorldTerrain"),
+  "Esri WorldShadedRelief": L.tileLayer.provider("Esri.WorldShadedRelief"),
+  "Esri WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
+  "Esri OceanBasemap": L.tileLayer.provider("Esri.OceanBasemap"),
+  "Esri NatGeoWorldMap": L.tileLayer.provider("Esri.NatGeoWorldMap"),
+  "Esri WorldGrayCanvas": L.tileLayer.provider("Esri.WorldGrayCanvas"),
+  "Geoportail France Maps": L.tileLayer.provider("GeoportailFrance"),
+  "Geoportail France Orthos": L.tileLayer.provider("GeoportailFrance.orthos"),
+  "Geoportail France classic maps": L.tileLayer.provider(
+    "GeoportailFrance.ignMaps"
+  )
 };
 
 // overlays
 var overlayLayers = {
-    'OpenSeaMap': L.tileLayer.provider('OpenSeaMap'),
-    'OpenWeatherMap Clouds': L.tileLayer.provider('OpenWeatherMap.Clouds'),
-    'OpenWeatherMap CloudsClassic': L.tileLayer.provider('OpenWeatherMap.CloudsClassic'),
-    'OpenWeatherMap Precipitation': L.tileLayer.provider('OpenWeatherMap.Precipitation'),
-    'OpenWeatherMap PrecipitationClassic': L.tileLayer.provider('OpenWeatherMap.PrecipitationClassic'),
-    'OpenWeatherMap Rain': L.tileLayer.provider('OpenWeatherMap.Rain'),
-    'OpenWeatherMap RainClassic': L.tileLayer.provider('OpenWeatherMap.RainClassic'),
-    'OpenWeatherMap Pressure': L.tileLayer.provider('OpenWeatherMap.Pressure'),
-    'OpenWeatherMap PressureContour': L.tileLayer.provider('OpenWeatherMap.PressureContour'),
-    'OpenWeatherMap Wind': L.tileLayer.provider('OpenWeatherMap.Wind'),
-    'OpenWeatherMap Temperature': L.tileLayer.provider('OpenWeatherMap.Temperature'),
-    'OpenWeatherMap Snow': L.tileLayer.provider('OpenWeatherMap.Snow'),
-    'Geoportail France Parcels': L.tileLayer.provider('GeoportailFrance.parcels')
+  OpenSeaMap: L.tileLayer.provider("OpenSeaMap"),
+  "OpenWeatherMap Clouds": L.tileLayer.provider("OpenWeatherMap.Clouds"),
+  "OpenWeatherMap CloudsClassic": L.tileLayer.provider(
+    "OpenWeatherMap.CloudsClassic"
+  ),
+  "OpenWeatherMap Precipitation": L.tileLayer.provider(
+    "OpenWeatherMap.Precipitation"
+  ),
+  "OpenWeatherMap PrecipitationClassic": L.tileLayer.provider(
+    "OpenWeatherMap.PrecipitationClassic"
+  ),
+  "OpenWeatherMap Rain": L.tileLayer.provider("OpenWeatherMap.Rain"),
+  "OpenWeatherMap RainClassic": L.tileLayer.provider(
+    "OpenWeatherMap.RainClassic"
+  ),
+  "OpenWeatherMap Pressure": L.tileLayer.provider("OpenWeatherMap.Pressure"),
+  "OpenWeatherMap PressureContour": L.tileLayer.provider(
+    "OpenWeatherMap.PressureContour"
+  ),
+  "OpenWeatherMap Wind": L.tileLayer.provider("OpenWeatherMap.Wind"),
+  "OpenWeatherMap Temperature": L.tileLayer.provider(
+    "OpenWeatherMap.Temperature"
+  ),
+  "OpenWeatherMap Snow": L.tileLayer.provider("OpenWeatherMap.Snow"),
+  "Geoportail France Parcels": L.tileLayer.provider("GeoportailFrance.parcels")
 };
 
 // adding baselayers and overlays to map
-L.control.layers(baseLayers, overlayLayers, {collapsed: true}).addTo(map);
+L.control.layers(baseLayers, overlayLayers, { collapsed: true }).addTo(map);
 
 // adding the sidebar to the map
-L.control.sidebar('sidebar').addTo(map);
-var legend = L.control({position: 'bottomright'});
+L.control.sidebar("sidebar").addTo(map);
+var legend = L.control({ position: "bottomright" });
 
 // all the layers go into this layer group
 var layerGroup = L.layerGroup().addTo(map);
@@ -175,32 +216,30 @@ legend.onAdd = function(map) {
 
 legend.addTo(map);
 
-
-// adding markers for identified cases 
-yde = L.marker([3.8480, 11.5021]);
+// adding markers for identified cases
+yde = L.marker([3.848, 11.5021]);
 yde.bindPopup("6 Cases Identifier ici");
-yde.on('mouseover',function(ev) {
-    yde.openPopup();
-  });
+yde.on("mouseover", function(ev) {
+  yde.openPopup();
+});
 yde.addTo(map);
-// adding markers for identified cases 
+// adding markers for identified cases
 baf = L.marker([5.4816, 10.4271]);
 baf.bindPopup("1 Cases Identifier ici");
-baf.on('mouseover',function(ev) {
-    baf.openPopup();
-  });
-  
+baf.on("mouseover", function(ev) {
+  baf.openPopup();
+});
+
 baf.addTo(map);
-// adding markers for identified cases 
+// adding markers for identified cases
 dla = L.marker([4.0511, 9.7679]);
 dla.bindPopup("9 Cases Identifier ici");
-dla.on('mouseover',function(ev) {
-    dla.openPopup();
-  });
+dla.on("mouseover", function(ev) {
+  dla.openPopup();
+});
 dla.addTo(map);
 
-
-// opening sidebar 
-$( document ).ready(function() {
-    document.getElementById('openSidebar').click();
+// opening sidebar
+$(document).ready(function() {
+  document.getElementById("openSidebar").click();
 });
